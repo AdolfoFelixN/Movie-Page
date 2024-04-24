61133dcc49a90e87091a8c0f46136582a928bfec
Adolfo Felix <164815910+AdolfoFelixN@users.noreply.github.com>
2024-04-23 21:40:12 -0600

    Second commit

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -6,7 +6,7 @@ export const Principal = ({sectionTitle}) => {
 
   useEffect(() => {
     fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=4acfd667d24272e7fdb51a14d37aaf96"
+      "https://api.themoviedb.org/3/movie/popular?api_key=4acfd667d24272e7fdb51a14d37aaf96&sort_by=popularity.desc&language=en-US"
     )
       .then((response) => response.json())
       .then((json) => {
@@ -24,7 +24,7 @@ export const Principal = ({sectionTitle}) => {
           <div className="movies">
             {data.map((valor) => (
               <div className="contenido" key={valor.id}>                
-                <img src={`https://image.tmdb.org/t/p/w500${valor.poster_path}`} alt="" />
+                <img src={`https://image.tmdb.org/t/p/w500${valor.poster_path}`} alt={`${valor.title}`} />
                 <li key={valor.id}>{valor.title}</li>
               </div>
               
--- src/components/SideOptions.jsx
+++ src/components/SideOptions.jsx
@@ -1,33 +1,44 @@
-import '../styles/sideOptions.css'
+import "../styles/sideOptions.css";
+
+const Li = ({ className, textAnchor }) => {
+  return (
+    <li>
+      <i className={className}></i>
+      <a href="#">{textAnchor}</a>
+    </li>
+  );
+};
+
+const buttonHide = () => {
+  alert('Esconder Lateral')
+}
 
 export const SideOptions = () => {
   return (
     <div className="options">
+      <div className="title">
         <h2>MoviesWeb</h2>
+        <hr />
+      </div>
+      <div className="main-options">
+        <button id="hide" onClick={buttonHide}><i className="bi bi-arrow-left-circle"></i></button>
+        <p>Main</p>
         <ul>
-            <li> 
-              <i className="bi bi-house-fill"></i> 
-              <a href="#"> Home </a>
-            </li>
-            <li> 
-              <i className="bi bi-camera-reels-fill"></i> 
-              <a href="#"> Movies </a>
-            </li>
-            <li> 
-              <i className="bi bi-film"></i> 
-              <a href="#"> Series </a>
-            </li>
-            <li> 
-              <i className="bi bi-collection-play-fill"></i> 
-              <a href="#"> Animes </a>
-            </li>
-            <li>
-              <i className="bi bi-robot"></i>
-              <a href="#">Cartoons</a></li>
-            <li>
-              <i className="bi bi-emoji-smile-fill"></i>
-              <a href="#">Kids</a></li>
+          <Li className={"bi bi-house-fill"} textAnchor={"Home"} />
+          <Li className={"bi bi-camera-reels-fill"} textAnchor={"Movies"} />
+          <Li className={"bi bi-film"} textAnchor={"Series"} />
+          <Li className={"bi bi-collection-play-fill"} textAnchor={"Animes"} />
+          <Li className={"bi bi-robot"} textAnchor={"Cartoons"} />
+          <Li className={"bi bi-emoji-smile-fill"} textAnchor={"Kids"} />
         </ul>
+      </div>
+      <div className="more-options">
+        <p>More</p>
+        <ul>
+          <Li className={"bi bi-gear"} textAnchor={"Settings"} />
+          <Li className={"bi bi-question-circle"} textAnchor={"Help"} />
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
