bbd7719e8d2542e9adb8b487bc4eabeb5aea86bb
Adolfo Felix <164815910+AdolfoFelixN@users.noreply.github.com>
2024-04-27 14:26:26 -0600

    fifth commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,30 @@
-import React from "react";
-import '../styles/modal.css'
+import React, { useState } from "react";
+import "../styles/modal.css";
 
-export const Modal = ({title, description, image, vote}) => {
+export const Modal = ({ title, description, image, vote, onClose }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="modal">
-
       <div className="imageAndDescription">
         <img src={image} alt={`${title}`} />
         <div className="description">
-            <h3>{title}</h3>
-            <p><strong>Film rating:</strong> {vote}</p>
-            <p><strong>Description:</strong> {description}</p>
+          <div className="closeButton">            
+            <i className="bi bi-x-square-fill closeButton" onClick={onClose}></i>
+          </div>
+          <h3>{title}</h3>
+          <p>
+            <strong>Film rating:</strong> {vote}
+          </p>
+          <p>
+            <strong>Description:</strong> {description}
+          </p>
         </div>
-        
-      </div>      
+      </div>
     </div>
   );
 };
--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,56 +1,84 @@
-import '../styles/principal.css'
+import "../styles/principal.css";
 import { useEffect, useState } from "react";
-import { Controls } from './Controls';
-import { Modal } from './Modal';
+import { Controls } from "./Controls";
+import { Modal } from "./Modal";
 
-export const Principal = ({sectionTitle}) => {
-  const [data, setData] = useState([])
-  const [page, setPage] = useState(1)
+export const Principal = ({ sectionTitle }) => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [movieSelected, setMovieSelected] = useState({
+    title: "",
+    imageUrl: "",
+    description: "",
+    filmRating: "",
+  });
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=4acfd667d24272e7fdb51a14d37aaf96&sort_by=popularity.desc&language=en-US&page=${page}` 
+      `https://api.themoviedb.org/3/movie/popular?api_key=4acfd667d24272e7fdb51a14d37aaf96&sort_by=popularity.desc&language=en-US&page=${page}`
     )
       .then((response) => response.json())
       .then((json) => {
-        setData(json.results);              
+        setData(json.results);
       });
-    
+    setShowModal(false);
   }, [page]);
 
-  
-  const [movieSelected, setMovieSelected] = useState({title: "", imageUrl: "" ,description: "", filmRating: ""})
-  console.log(data)
+  const handleMovieClick = (valor) => {
+    setMovieSelected({
+      title: valor.title,
+      imageUrl: `https://image.tmdb.org/t/p/w500${valor.poster_path}`,
+      description: valor.overview,
+      filmRating: valor.vote_average,
+    });
+    setShowModal(true); // Mostrar el modal cuando se selecciona una película
+  };
+
+  const closeModal = () => {
+    setMovieSelected({
+      title: "",
+      imageUrl: "",
+      description: "",
+      filmRating: "",
+    });
+    setShowModal(false);
+  };
 
   return (
     <>
-      <main className='moviesSection'>      
+      <main className="moviesSection">
         <div className="movies-container">
           <p>{sectionTitle}</p>
           <div className="movies">
             {data.map((valor) => (
-              <div className="contenido" key={valor.id} onClick={ () => setMovieSelected({title: valor.title, imageUrl: `https://image.tmdb.org/t/p/w500${valor.poster_path}`, description: valor.overview, filmRating: valor.vote_average})}>                 
-                <img src={`https://image.tmdb.org/t/p/w500${valor.poster_path}`} alt={`${valor.title}`} />
+              <div
+                className="contenido"
+                key={valor.id}
+                onClick={() => handleMovieClick(valor)}
+              >
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${valor.poster_path}`}
+                  alt={valor.title}
+                />
                 <li key={valor.id}>{valor.title}</li>
               </div>
-              
             ))}
           </div>
         </div>
-
-        <Modal 
-          title={movieSelected.title}
-          description={movieSelected.description}
-          image={movieSelected.imageUrl}     
-          vote={movieSelected.filmRating}     
-        />
-        
+        {/* Mostrar el modal si showModal es true */}
+        {showModal && (
+          <Modal
+            title={movieSelected.title}
+            description={movieSelected.description}
+            image={movieSelected.imageUrl}
+            vote={movieSelected.filmRating}
+            onClose={closeModal} // Proporciona una función para cerrar el modal
+          />
+        )}
       </main>
 
-      <Controls page={page} setPage={setPage}/>
+      <Controls page={page} setPage={setPage} />
     </>
   );
-
-  
-
 };
